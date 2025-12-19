4b748ab456ce86ac3d6985157558e3777850bb1d
André Arko <andre@arko.net>
2025-12-19 16:02:48 +0700

    add relay and local for obn2

--- src/test/java/com/example/demo/Http2StreamingExample.java
+++ src/test/java/com/example/demo/Http2StreamingExample.java
@@ -10,14 +10,17 @@ class DemoApplicationTests {
 		// OBN_URL : Relay endpoints that stream traffic via relay to backend server.
 		final String SPC_URL = "https://token-ohttp-dev.arcane.samsungspc.cloud/ogw";
 		final String OBN_URL = "https://relay.oblivious.network/dreamy-snowflake-12";
+		final String OB2_URL = "https://relay2.oblivious.network/dreamy-snowflake-12";
 		final String LOCAL_SPC = "https://localhost:9100/ogw";
 		final String LOCAL_OBN = "https://localhost:9000/dreamy-snowflake-12";
 		final String LOCAL_DEV = "https://localhost:9200/dreamy-snowflake-12";
 		final String TEST_FLY = "https://fullduplex.fly.dev/500";
 		final String TEST_RELAY = "https://relay.oblivious.network/fullduplex-500";
+		final String LOCAL_OB2 = "http://127.0.0.1:7676/dreamy-snowflake-12";
 
-		Http2StreamingExample.main(SPC_URL);
-		Http2StreamingExample.main(OBN_URL);
-		Thread.sleep(5000);
+		// Http2StreamingExample.main(SPC_URL);
+		Http2StreamingExample.main(OB2_URL);
+		// Http2StreamingExample.main(LOCAL_OB2);
+		Thread.sleep(10000);
 	}
 }
